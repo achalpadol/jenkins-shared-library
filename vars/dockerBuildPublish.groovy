382841eb8b587c2dd6ab058a6c0d3e1bde14508b
Achal Sunil Padol <achalpadol@gmail.com>
2026-09-17 14:37:30 +0530

    Update dockerBuildPublish.groovy

--- vars/dockerBuildPublish.groovy
+++ vars/dockerBuildPublish.groovy
@@ -1,1 +1,32 @@
+def call(Map config) {
 
+    def app
+
+    String image = config.image
+    String credential = config.credential
+    String registry = config.registry ?: 'https://index.docker.io/v1/'
+
+    stage('Docker Build') {
+        script {
+            echo "Building Docker image: ${image}:${env.BUILD_NUMBER}"
+
+            app = docker.build(
+                "${image}:${env.BUILD_NUMBER}"
+            )
+        }
+    }
+
+    stage('Docker Publish') {
+        script {
+            echo "Publishing Docker image..."
+
+            docker.withRegistry(
+                registry,
+                credential
+            ) {
+                app.push()
+                app.push("latest")
+            }
+        }
+    }
+}
